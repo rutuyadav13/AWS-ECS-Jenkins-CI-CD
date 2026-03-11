4a79c6144c2874edb1b8095c02f18b17c0c3a9f8
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 22:18:33 +0530

    Update jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins" {
   ami           = "ami-0b6c6ebed2801a5cb"  # Ubuntu AMI
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "YOUR_KEY_PAIR"          # Replace with your key
+  key_name      = "jenkins key"          
 
   vpc_security_group_ids = [aws_security_group.ecs_sg.id]
 
