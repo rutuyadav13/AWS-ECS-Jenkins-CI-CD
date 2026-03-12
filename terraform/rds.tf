b27c436276655aab730fecdd16f3ca32b10247ce
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 13:37:09 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,21 +1,14 @@
-resource "aws_db_subnet_group" "rds_subnet" {
-  name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id]
-
-  tags = { Name = "RDS-Subnet-Group" }
-}
-
 resource "aws_db_instance" "rds" {
-  allocated_storage      = 20
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
-  username               = "admin"
-  password               = "StrongPassword123"
-  publicly_accessible    = false
+  identifier         = "ecs-jenkins-rds"
+  allocated_storage  = 20
+  engine             = "mysql"
+  engine_version     = "8.0"
+  instance_class     = "db.t3.micro"
+  name               = "mydb"   # only lowercase letters/numbers
+  username           = "admin"
+  password           = "StrongPassword123"
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
-  skip_final_snapshot    = true
-
-  tags = { Name = "RDS-MySQL" }
+  skip_final_snapshot = true
+  multi_az            = true
 }
