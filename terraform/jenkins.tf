03d98a6d68f06631fe03a4c907bf2c66853467cc
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 13:38:15 +0530

    Update jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -1,12 +1,16 @@
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0b6c6ebed2801a5cb"
+  ami           = "ami-0b6c6ebed2801a5cb"  # Ubuntu 22.04 LTS
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "jenkins key"
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  key_name      = "jenkins key"         
+  subnet_id     = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [
+    aws_security_group.jenkins_sg.id
+  ]
+  associate_public_ip_address = true
 
-  # FIX: correct relative path to user_data.sh
   user_data = file("${path.module}/../jenkins/user_data.sh")
 
-  tags = { Name = "Jenkins-Server" }
+  tags = {
+    Name = "Jenkins-Server"
+  }
 }
