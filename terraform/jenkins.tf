3a184610bb41de764111745decc710b4f807fa9c
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 07:00:00 +0000

    Update jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "jenkins" {
   ami           = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "YOUR_KEY_PAIR"
+  key_name      = "jenkins key"
   vpc_security_group_ids = [aws_security_group.ecs_sg.id]
 
   # Correct path to user_data.sh
