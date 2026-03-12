e71efcc473877220a7a49822ce1bcadcb1b969e3
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 13:45:47 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "rds" {
   engine             = "mysql"
   engine_version     = "8.0"
   instance_class     = "db.t3.micro"
-  name               = "mydb"   # only lowercase letters/numbers
+  db_name            = "mydb" 
   username           = "admin"
   password           = "StrongPassword123"
   db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
