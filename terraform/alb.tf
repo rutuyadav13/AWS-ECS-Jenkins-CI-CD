c65d087f14ffd3bcf35191d97b1397043c2fc912
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 08:02:37 +0000

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -4,7 +4,10 @@ resource "aws_lb" "alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_subnet.id]
+  subnets = [
+              aws_subnet.public_subnet_1.id,
+              aws_subnet.public_subnet_2.id
+            ]
 
   tags = {
     Name = "ecs-alb"
