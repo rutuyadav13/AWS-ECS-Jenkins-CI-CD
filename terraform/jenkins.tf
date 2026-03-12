43c635f10250931bca9092d83dd3591a5460960f
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 06:49:45 +0000

    Update jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -1,14 +1,11 @@
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0b6c6ebed2801a5cb"  # Ubuntu AMI
-  instance_type = "t3.micro"
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "jenkins key"          
-
+  key_name      = "YOUR_KEY_PAIR"
   vpc_security_group_ids = [aws_security_group.ecs_sg.id]
 
   user_data = file("${path.module}/jenkins/user_data.sh")
 
-  tags = {
-    Name = "Jenkins-Server"
-  }
+  tags = { Name = "Jenkins-Server" }
 }
