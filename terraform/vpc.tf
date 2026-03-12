6aee786f64dc325aee26ee7fc9f66af2888ae2ef
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 07:14:57 +0000

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = { Name = "private-subnet-1" }
 }
 
