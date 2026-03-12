71d9b0c92fa7d19a8277fb0ffb65c282bcb25dce
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-12 13:40:34 +0530

    Update user_data.sh

--- jenkins/user_data.sh
+++ jenkins/user_data.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# Install Java (required for Jenkins)
+# Update OS & install dependencies
 apt update -y
-apt install openjdk-11-jdk -y
+apt install -y openjdk-11-jdk git docker.io awscli
 
-# Add Jenkins repo and install
+# Start Docker
+systemctl enable docker
+systemctl start docker
+
+# Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
 sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt update -y
-apt install jenkins -y
-
-# Start Jenkins
-systemctl start jenkins
+apt install -y jenkins
 systemctl enable jenkins
+systemctl start jenkins
+
+# Login to AWS ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin YOUR_AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
